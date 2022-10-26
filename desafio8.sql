4b2786420858aac659bb8a5b3c501dd6474706f7
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-10-25 22:58:07 -0300

    Requisito 8 :game_die: :game_die:

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+SELECT 
+    ar.artist_name AS artista, al.album_name AS album
+FROM
+    SpotifyClone.artists AS ar
+        INNER JOIN
+    SpotifyClone.albums AS al ON ar.artist_id = al.artist_id
+WHERE
+    ar.artist_name = 'Elis Regina';
